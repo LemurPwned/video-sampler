fbb95773a4c95cd9e128081f26834eda61a8610e
LemurPwned <jakub.mojsiejuk@gmail.com>
2023-11-11 21:23:42 +0100

    test updates

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,18 +1,22 @@
 from video_sampler.sampler import SamplerConfig, VideoSampler
+from video_sampler.schemas import FrameObject
 from PIL import Image
-from itertools import chain
+from typing import List
+
+
+def verify_frame_res(res: List[FrameObject]):
+    for frame_obj in res:
+        if frame_obj.frame:
+            assert isinstance(frame_obj.frame, Image.Image)
+            assert isinstance(frame_obj.metadata, dict)
+            assert "frame_time" in frame_obj.metadata
 
 
 def test_pass_gating(base_video: str):
     config = SamplerConfig()
     sampler = VideoSampler(config)
-    for res in chain(sampler.sample(base_video)):
-        for frame, meta in res:
-            if frame:
-                assert isinstance(frame, Image.Image)
-                assert isinstance(meta, dict)
-                assert "frame_time" in meta
-
+    for res in sampler.sample(base_video):
+        verify_frame_res(res)
     stats = sampler.stats
     assert stats["produced"] == stats["gated"]
 
@@ -29,12 +33,8 @@ def test_clip_gating(base_video: str):
         neg_margin=0.3,
     )
     sampler = VideoSampler(config)
-    for res in chain(sampler.sample(base_video)):
-        for frame, meta in res:
-            if frame:
-                assert isinstance(frame, Image.Image)
-                assert isinstance(meta, dict)
-                assert "frame_time" in meta
+    for res in sampler.sample(base_video):
+        verify_frame_res(res)
 
 
 def test_grid(base_video: str):
@@ -43,9 +43,5 @@ def test_grid(base_video: str):
         type="grid", hash_size=4, size=30, max_hits=2, grid_x=4, grid_y=4, debug=True
     )
     sampler = VideoSampler(config)
-    for res in chain(sampler.sample(base_video)):
-        for frame, meta in res:
-            if frame:
-                assert isinstance(frame, Image.Image)
-                assert isinstance(meta, dict)
-                assert "frame_time" in meta
+    for res in sampler.sample(base_video):
+        verify_frame_res(res)
