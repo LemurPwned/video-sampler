3220517f9ded858e43bdfc55a72de3e942d56b47
LemurPwned <jakub.mojsiejuk@gmail.com>
2024-02-23 00:16:08 +0100

    adding assets for tests

--- tests/test_ytplugin.py
+++ tests/test_ytplugin.py
@@ -33,3 +33,23 @@ def test_segment_sampler(random_video):
         worker.launch(video_path=url, output_path=tempdir, pretty_video_name=title)
 
         assert len(os.listdir(tempdir)) > 0
+
+
+def test_single_url_gen(random_video):
+    ytdlp = YTDLPPlugin()
+    title, url, subs = next(ytdlp.generate_urls(random_video, get_subs=True))
+    assert title, f"Expected a title, got {title}"
+    assert url.endswith(".m3u8"), f"Expected a video URL, got {url}"
+    assert subs and len(subs) > 0, f"Expected subtitles, got {subs}"
+
+
+def test_search_url_gen():
+    ytdlp = YTDLPPlugin()
+    expected_results, results = 5, 0
+    search_url = f"ytsearch{expected_results}:cute cats"
+    for title, url, subs in ytdlp.generate_urls(search_url, get_subs=True):
+        assert title, f"Expected a title, got {title}"
+        assert url.endswith(".m3u8"), f"Expected a video URL, got {url}"
+        assert subs and len(subs) > 0, f"Expected subtitles, got {subs}"
+        results += 1
+    assert results == 5, f"Expected 5 results, got {results}"
